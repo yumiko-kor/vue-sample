a7f3888528ee349873c83a175e873528125bca8a
yugyoengJung <jyg@ngle.co.kr>
2023-03-16 17:58:03 +0900

    feat: 이벤트 핸들러 작성

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -6,12 +6,16 @@
       <a v-for="tab in menus" :key="tab">{{ tab }}</a>
     </div>
   
-    <div>
-      <h4>{{products[0]}}</h4>
+    <div v-for="(a, i) in products" :key="i">
+      <h4>{{a}}</h4>
       <p>{{price1}} 만원</p>
+      <button v-on:click="report++">허위매물신고</button> <span>{{ report }}</span>
+      <button @click="report+=1">허위매물신고</button> <span>{{ report }}</span>
+      <button @click="increase()">허위매물신고</button> <span>{{ report }}</span>
     </div>
-  
-    <div>
+
+
+    <div >
       <h4>{{products[1]}}</h4>
       <p>{{price2}} 만원</p>
     </div>
@@ -20,8 +24,6 @@
       <h4>{{products[2]}}</h4>
       <p>{{price2}} 만원</p>
     </div>
-
-
 
 
   </div>
@@ -45,12 +47,18 @@ export default {
       price2 : 90,
 
 
+      report: 0,
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동 원룸', '천호동 원룸', '잠실동 원룸']
     }
   },
   components: {
+  },
 
+  methods : { 
+    increase(){ 
+      this.report += 1 
+    } 
   }
 }
 </script>
