73c8893f0b2234f96d3f78ced2fe1b310b7cbd83
yugyoengJung <jyg@ngle.co.kr>
2023-03-20 11:37:03 +0900

    feat: 모달창 만들기

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
 
+    <div class="black-bg" v-if="modal == true" @click="close()">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>Information Text</p>
+      </div>
+    </div>
 
     <div class="menu">
       <a v-for="tab in menus" :key="tab">{{ tab }}</a>
@@ -49,7 +55,10 @@ export default {
 
       report: 0,
       menus : ['Home', 'Shop', 'About'],
-      products : ['역삼동 원룸', '천호동 원룸', '잠실동 원룸']
+      products : ['역삼동 원룸', '천호동 원룸', '잠실동 원룸'],
+
+      // 모달창
+      modal: true
     }
   },
   components: {
@@ -58,19 +67,37 @@ export default {
   methods : { 
     increase(){ 
       this.report += 1 
-    } 
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+  margin-top: 50%;
+} 
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
 .menu {
