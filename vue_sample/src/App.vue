3e9a5074dbd5d05e176281dd2913b3e79a8b140e
yugyoengJung <jyg@ngle.co.kr>
2023-03-20 18:51:59 +0900

    feat: 모달창 만들기

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
 
-    <div class="black-bg" v-if="modal == true" @click="close()">
+    <div class="black-bg" v-if="modal == true" @click="modal = false">
       <div class="white-bg">
         <h4>상세페이지</h4>
         <p>Information Text</p>
-      </div>
+      </div> 
     </div>
 
     <div class="menu">
       <a v-for="tab in menus" :key="tab">{{ tab }}</a>
     </div>
   
-    <div v-for="(a, i) in products" :key="i">
-      <h4>{{a}}</h4>
+    <div class="room">
+      <img src="./assets/room/room0.jpg">
+      <h4 @click="modal=true">{{ products[0] }}</h4>
       <p>{{price1}} 만원</p>
-      <button v-on:click="report++">허위매물신고</button> <span>{{ report }}</span>
-      <button @click="report+=1">허위매물신고</button> <span>{{ report }}</span>
-      <button @click="increase()">허위매물신고</button> <span>{{ report }}</span>
+      <button @click="report[0]++">허위매물신고</button> <span>{{ report[0] }}</span>
+    </div>
+    
+
+    <div class="room">
+      <img src="./assets/room/room1.jpg">
+      <h4>{{ products[1] }}</h4>
+      <p>{{price1}} 만원</p>
+      <button @click="report[1]++">허위매물신고</button> <span>{{ report[1] }}</span>
     </div>
 
-
-    <div >
-      <h4>{{products[1]}}</h4>
-      <p>{{price2}} 만원</p>
-    </div>
-  
-    <div>
-      <h4>{{products[2]}}</h4>
-      <p>{{price2}} 만원</p>
+    <div class="room">
+      <img src="./assets/room/room2.jpg">
+      <h4>{{ products[2] }}</h4>
+      <p>{{price1}} 만원</p>
+      <button @click="report[2]++">허위매물신고</button> <span>{{ report[2] }}</span>
     </div>
 
 
@@ -53,12 +56,12 @@ export default {
       price2 : 90,
 
 
-      report: 0,
+      report: [0,0,0],
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동 원룸', '천호동 원룸', '잠실동 원룸'],
 
       // 모달창
-      modal: true
+      modal: false
     }
   },
   components: {
@@ -76,20 +79,28 @@ export default {
 body {
   margin : 0;
 }
+
 div {
   box-sizing: border-box;
 }
+
 .black-bg {
   width: 100%; height:100%;
   background: rgba(0,0,0,0.5);
   position: fixed; padding: 20px;
 }
+
 .white-bg {
   width: 100%; background: white;
   border-radius: 8px;
   padding: 20px;
   margin-top: 50%;
 } 
+
+.room img {
+  width: 85%;
+  margin-top: 40px;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
