a2dd3bc550c9bf52823090a5886b20fc5b12c305
yugyoengJung <jyg@ngle.co.kr>
2023-03-22 11:27:12 +0900

    feat: 콘텐츠 반복문 처리

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
 
+    <!-- <div class="black-bg" v-if="modal == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>Information Text</p>
+        <button @click="modal = false">창 닫기</button>
+      </div> 
+    </div> -->
+
     <div class="black-bg" v-if="modal == true">
       <div class="white-bg">
         <h4>상세페이지</h4>
@@ -9,34 +17,17 @@
       </div> 
     </div>
 
+
     <div class="menu">
       <a v-for="tab in menus" :key="tab">{{ tab }}</a>
     </div>
   
-    <div class="room">
-      <img :src="products[0].image">
-      <!-- html 태그 안의 속성에서 데이터 바인딩을 하고 싶으면 : 세미콜론을 붙여주면 된다. -->
-
-      <h4 @click="modal=true">{{ products[0].title }}</h4>
-      <p>{{ products[0].content }}</p>
-      <p class="gray">{{ products[0].price }}원</p>
-      <button @click="report[0]++">허위매물신고</button> <span>{{ report[0] }}</span>
-    </div>
-    
-    <div class="room">
-      <img :src="products[1].image">
-      <h4 @click="modal=true">{{ products[1].title }}</h4>
-      <p>{{ products[1].content }}</p>
-      <p class="gray">{{ products[1].price }}원</p>
-      <button @click="report[1]++">허위매물신고</button> <span>{{ report[1] }}</span>
-    </div>
-
-    <div class="room">
-      <img :src="products[2].image">
-      <h4 @click="modal=true">{{ products[2].title }}</h4>
-      <p>{{ products[2].content }}</p>
-      <p class="gray">{{ products[2].price }}원</p>
-      <button @click="report[2]++">허위매물신고</button> <span>{{ report[2] }}</span>
+    <div class="room" v-for="(room, i) in products" :key="i">
+      <img :src="products[i].image">
+      <h4 @click="modal=true">{{ products[i].title }}</h4>
+      <p>{{ products[i].content }}</p>
+      <p class="gray">{{ products[i].price }}원</p>
+      <button @click="products[i].report++">허위매물신고</button> <span>{{products[i].report }}</span>
     </div>
 
   </div>
